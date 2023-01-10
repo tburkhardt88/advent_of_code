1a123d172a42dee766678885a0f11e4ded96f92c
tburkhardt88 <83732608+tburkhardt88@users.noreply.github.com>
2023-01-10 14:36:04 +1300

    Update output.R

--- D15/output.R
+++ D15/output.R
@@ -18,26 +18,19 @@ beacons <- lapply(beacons, function(bcs){
   }, simplify = T, USE.NAMES = F)
 })
 
-#In dataframe format
-locations <- data.frame(
-  sensor_x = sapply(sensors, function(sns){sns[1]}),
-  sensor_y = sapply(sensors, function(sns){sns[2]}),
-  beacon_x = sapply(beacons, function(sns){sns[1]}),
-  beacon_y = sapply(beacons, function(sns){sns[2]})
-)
-locations$distance = abs(locations$sensor_x - locations$beacon_x) + abs(locations$sensor_y - locations$beacon_y)
+#Distances 
+distances <- numeric(length = length(sensors))
+for(i in 1:length(sensors)){distances[i] <- sum(abs(sensors[[i]] - beacons[[i]]))}
 
 #Testing row 2000000 (Part 1)
-x_coords <- seq(min(locations$sensor_x - locations$distance), max(locations$sensor_x + locations$distance))
-coords_to_test <- cbind(
-  as.matrix(x_coords),
-  as.matrix(rep(2000000, length(x_coords)))
-)
+x_coords <- seq(min(sapply(1:length(sensors), function(i){sensors[[i]][1] - distances[i]})), 
+                max(sapply(1:length(sensors), function(i){sensors[[i]][1] + distances[i]})))
+
 no_beacon = logical(length = length(x_coords))
 
 for(i in 1:length(no_beacon)){
-  for(s in 1:nrow(locations)){
-    if(sum(abs(sensors[[s]] - coords_to_test[i,])) <= locations$distance[s]){
+  for(s in 1:length(sensors)){
+    if(sum(abs(sensors[[s]] - c(x_coords[i], 2E6))) <= distances[s]){
       no_beacon[[i]] <- TRUE
       break
     }
@@ -47,3 +40,34 @@ for(i in 1:length(no_beacon)){
 beacons_on_line_2E6 = unique(locations[locations$beacon_y == 2E6,3:4])
 
 length(which(no_beacon)) - nrow(beacons_on_line_2E6)
+
+
+#Part 2
+perimeters <- list()
+for(i in 1:length(sensors)){
+  perimeter_x <- seq(sensors[[i]][1] - distances[i] - 1, sensors[[i]][1] + distances[i] + 1) #add and subtract 1 to get outside perimeter
+  perimeter_y_positive <- sensors[[i]][2] + ((distances[i] + 1) - abs(perimeter_x - sensors[[i]][1]))
+  perimeter_y_negative <- sensors[[i]][2] - ((distances[i] + 1) - abs(perimeter_x - sensors[[i]][1]))
+  perimeters[[i]] <- rbind(cbind(perimeter_x, perimeter_y_positive), cbind(perimeter_x, perimeter_y_negative))
+}
+rm(list = c('perimeter_y_negative','perimeter_y_positive','perimeter_x','i'))
+
+check_coords <- perimeters[[1]]
+for(i in 2:length(perimeters)){
+  check_coords <- rbind(check_coords, perimeters[[i]])
+}
+
+check_coords <- check_coords[check_coords[,1] <= 4E6 & check_coords[,1] >= 0 & check_coords[,2] <= 4E6 & check_coords[,2] >= 0,] #confine to within the 0 - 4E6 box
+
+unchecked = rep(T, nrow(check_coords))
+for(i in 1:length(unchecked)){
+  for(s in 1:length(sensors)){
+    if(sum(abs(sensors[[s]] - check_coords[i,])) <= distances[s]){
+      unchecked[[i]] <- F
+      break
+    }
+  }
+}
+
+distress_beacon <- unique(check_coords[which(unchecked),])
+format(distress_beacon[1] * 4E6 + distress_beacon[2], scientific = F)
